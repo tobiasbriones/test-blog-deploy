9adf0b7b6241b9dfa23aa1c08db507f79863165e
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-09-01 21:08:42 -0600

    add article

---
+++ mathswe/representation/repsymo/2dp/mrm/feat/drawing-a-tree-on-canvas-with-xy-coordinates/web/src/main.ts
@@ -0,0 +1,54 @@
+// Copyright (c) 2022 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: MIT
+// This file is part of https://github.com/mathsoftware/engineer
+
+import './index.html';
+import './main.css';
+import { newTreeNode, TreeNode } from './model';
+import { SolutionTreeCanvas } from './mrm-canvas';
+
+main().then(console.log);
+
+async function main() {
+  const path = 'root-node.json';
+  const tree = await fetchTree(path);
+
+  window.onresize = () => render(tree);
+  render(tree);
+  return 'Initialized';
+}
+
+function render(tree: TreeNode) {
+  const canvasEl = document.getElementById('solutionTree') as HTMLCanvasElement;
+  const canvas = new SolutionTreeCanvas();
+  canvas.rootNode = tree;
+
+  canvas.init(canvasEl);
+  canvas.render();
+}
+
+function fetchTree(path: string): Promise<TreeNode> {
+  return fetch(path)
+    .then(res => res.ok ? res : Promise.reject(res.statusText))
+    .then(res => res.json())
+    .catch(reason => {
+      showError({ reason, msg: 'Failed to fetch tree' });
+      console.error(reason);
+      return newTreeNode();
+    });
+}
+
+interface Error {
+  reason: any;
+  msg: string;
+}
+
+function showError(error: Error) {
+  document.getElementById('error').innerText = errorMsg(error);
+  return error;
+}
+
+function errorMsg(error: Error): string {
+  const { reason, msg } = error;
+  return `${ msg }: ${ reason }`;
+}
